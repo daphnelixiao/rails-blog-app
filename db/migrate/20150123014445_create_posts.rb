d95cecadb3d63e6efa68e19de6f25e45409c9b33
Casey - Product School <Casey-ProductSchool@Macintosh-2.att.net>
2015-01-22 18:22:11 -0800

    Removed blog related mvc functionality. App is back to basic configuration

--- db/migrate/20150123014445_create_posts.rb
+++
@@ -1,7 +0,0 @@
-class CreatePosts < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :title
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123014527) do
+ActiveRecord::Schema.define(version: 20150123010144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
